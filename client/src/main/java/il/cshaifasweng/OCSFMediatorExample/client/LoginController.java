0a735dd4df5cc4d1a5fa059251d577c22cee78d3
Baseem Salem <baseem.casillias.1@hotmail.co.uk>
2023-01-12 15:39:47 +0200

    Making fxml, css, java files in Client.

---
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/LoginController.java
@@ -0,0 +1,16 @@
+package il.cshaifasweng.OCSFMediatorExample.client;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+    public ChoiceBox acc_selector;
+    public Label acc_id_lbl;
+    public TextField acc_id_fld;
+    public Label password_lbl;
+    public TextField password_fld;
+    public Button login_btn;
+    public Label error_lbl;
+}
