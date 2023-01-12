c1694a3a4d0ad0e4564d44e8cae25fedd7b0b743
Ehab-Ma <mansourehab34@gmail.com>
2023-01-12 20:17:29 +0200

    Writing new fxml and java file

---
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/MakeAppointmentController.java
@@ -0,0 +1,14 @@
+package il.cshaifasweng.OCSFMediatorExample.client;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.text.Text;
+
+public class MakeAppointmentController {
+    public Text hello_user_lbl;
+    public DatePicker date_selector_cl;
+    public ChoiceBox time_selector;
+    public Button confirm_app_btn;
+    public Text app_status_txt;
+}
