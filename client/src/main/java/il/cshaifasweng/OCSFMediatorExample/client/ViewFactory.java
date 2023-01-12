265a2c55b8b63847e94e8ba1685dae15091826b1
Ehab-Ma <mansourehab34@gmail.com>
2023-01-12 20:48:49 +0200

    update new fxml and java file

---
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/ViewFactory.java
@@ -0,0 +1,55 @@
+package il.cshaifasweng.OCSFMediatorExample.client;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class ViewFactory {
+    //Client Views
+     private AnchorPane makeAppointmentView;
+     public ViewFactory(){};
+
+     public AnchorPane getMakeAppointmentView() {
+          if(makeAppointmentView == null)
+          {
+               try {
+                    makeAppointmentView = new FXMLLoader(getClass().getResource("/il.cshaifasweng.OCSFMediatorExample.client/MakeAppointment.fxml")).load();
+               } catch (Exception e)
+               {
+                    e.printStackTrace();
+               }
+          }
+          return makeAppointmentView;
+     }
+
+     public void showLoginWindow(){
+          FXMLLoader loader = new FXMLLoader(getClass().getResource("/il.cshaifasweng.OCSFMediatorExample.client/Login.fxml"));
+          Scene scene = null;
+          try {
+              scene = new Scene(loader.load());
+          } catch (Exception e) {
+              e.printStackTrace();
+          }
+         Stage stage = new Stage();
+          stage.setScene(scene);
+          stage.setTitle("Health Center");
+          stage.show();
+     }
+
+     public void showPatientWindow() {
+         FXMLLoader loader = new FXMLLoader(getClass().getResource("/il.cshaifasweng.OCSFMediatorExample.client/Patient.fxml"));
+         PatientController patientController = new PatientController();
+         loader.setController(patientController);
+         Scene scene = null;
+         try {
+             scene = new Scene(loader.load());
+         } catch (Exception e) {
+             e.printStackTrace();
+         }
+         Stage stage = new Stage();
+         stage.setScene(scene);
+         stage.setTitle("Health Center");
+         stage.show();
+     }
+}
