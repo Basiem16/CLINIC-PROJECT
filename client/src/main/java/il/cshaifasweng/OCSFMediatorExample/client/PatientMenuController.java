ad80b8dc44733d8a8af75487024a2e9da5160124
Baseem Salem <baseem.casillias.1@hotmail.co.uk>
2023-01-12 17:22:25 +0200

    Updating fxml, css files in Client.

--- client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/PatientMenuController.java
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/PatientMenuController.java
@@ -1,4 +1,20 @@
 package il.cshaifasweng.OCSFMediatorExample.client;
 
-public class PatientMenuController {
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PatientMenuController implements Initializable {
+    public Button make_app_btn;
+    public Button show_app_btn;
+    public Button profile_btn;
+    public FontAwesomeIconView logout_btn;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
