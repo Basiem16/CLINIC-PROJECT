a3711743e3e633fa1f8a7810c20d2375df8ccf56
Ehab-Ma <mansourehab34@gmail.com>
2023-01-12 23:18:58 +0200

    update new fxml and java file

--- client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/LoginController.java
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/LoginController.java
@@ -20,6 +20,9 @@ public class LoginController implements Initializable {
     @FXML // URL location of the FXML file that was given to the FXMLLoader
     private URL location;
 
+    @FXML
+    private Button Magnetic_Station_btn;
+
     @FXML // fx:id="acc_id_fld"
     private TextField acc_id_fld; // Value injected by FXMLLoader
 
@@ -58,6 +61,7 @@ public class LoginController implements Initializable {
         assert login_btn != null : "fx:id=\"login_btn\" was not injected: check your FXML file 'Login.fxml'.";
         assert password_fld != null : "fx:id=\"password_fld\" was not injected: check your FXML file 'Login.fxml'.";
         assert password_lbl != null : "fx:id=\"password_lbl\" was not injected: check your FXML file 'Login.fxml'.";
+        assert Magnetic_Station_btn != null : "fx:id=\"Magnetic_Station_btn\" was not injected: check your FXML file 'Login.fxml'.";
 
     }
 
